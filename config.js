a7737ffc2adc15fe5e089c8ba2a5aa5628c97aa1
MCES10 <138902870+MCES10@users.noreply.github.com>
2023-12-12 20:23:39 +0000

    main

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAlHDidKtotbYgfgVRVbmQNyaftWJkVDlU",
+    authDomain: "mces10-software.firebaseapp.com",
+    projectId: "mces10-software",
+    storageBucket: "mces10-software.appspot.com",
+    messagingSenderId: "41062100813",
+    appId: "1:41062100813:web:2f5834c406d3f071b2d104",
+    measurementId: "G-9WPL9BQ08S"
+  };
